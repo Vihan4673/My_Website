ff087a5792eb787038b78e7989cc5a57bc435909
vihan <vihanvimen46@gmail.com>
2025-10-07 10:12:52 +0530

    Home page design

---
+++ js.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed('.typing', {
+        strings: [
+            "Tech Leader",
+            "Software Engineer",
+            "DevOps Specialist",
+            "Educator",
+            "Entrepreneur",
+            "Content Creator"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
